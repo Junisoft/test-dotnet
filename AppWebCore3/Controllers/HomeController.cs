5b490f6142a2a350435281e4c37efc82b5e1e2c8
Junior Antonio Alvarez Diestra <j_alvarez95@hotmail.com>
2019-01-02 13:55:39 -0500

    segundo commit

---
+++ AppWebCore3/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppWebCore3.Models;
+
+namespace AppWebCore3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        //[Route("")]
+        public IActionResult Empleados()
+        {        
+            ViewBag.Empleados = db.Empleados.ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("Add", new Empleados());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Empleados emp)
+        {
+            db.Add(emp);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppWebCore3/Models/Empleados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppWebCore3.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int id_emp { get; set; } //SongId or Id asi automaticamente lo reconoce como PK
+        public string nombre_emp { get; set; }
+        public string apellidos_emp { get; set; }
+        public string salario_emp { get; set; }
+    }
+}
